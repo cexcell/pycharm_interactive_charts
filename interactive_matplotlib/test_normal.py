8289a957571b847ca277fe182182ed273786642a
Andrey Lazarevich <thecexcell@gmail.com>
2015-03-30 18:25:46 +0300

    starting to rewrite interactive backend

--- interactive_matplotlib/test_normal.py
+++ interactive_matplotlib/test_normal.py
@@ -3,8 +3,8 @@ from matplotlib import pyplot as plt
 plt.plot([1, 2, 3, 4, 5, 6])
 plt.show()
 
-plt.plot([1, 2, 3, 4, 5], [1, 4, 9, 16, 25])
-plt.show()
-
-plt.plot([1, 2, 3, 4, 5, 6], [1, 1, 1, 1, 1, 1])
-plt.show()
+# plt.plot([1, 2, 3, 4, 5], [1, 4, 9, 16, 25])
+# plt.show()
+#
+# plt.plot([1, 2, 3, 4, 5, 6], [1, 1, 1, 1, 1, 1])
+# plt.show()
