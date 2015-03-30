9b8296b386a57fadf0af9d9e29eb1e474d219bdd
Andrey Lazarevich <thecexcell@gmail.com>
2015-03-30 20:34:45 +0300

    prettify simple backend version
    Now backend do it work.
    Modifications waiting

--- interactive_matplotlib/interactive_matplotlib/backend_interagg.py
+++ interactive_matplotlib/interactive_matplotlib/backend_interagg.py
@@ -1,8 +1,13 @@
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.backend_bases import FigureManagerBase, ShowBase
 from matplotlib._pylab_helpers import Gcf
+from matplotlib.figure import Figure
 import matplotlib
+import os
 
+
+NAME_PREFIX = ".idea/charts/picharts"
+CHART_DIR = ".idea/charts"
 
 rcParams = matplotlib.rcParams
 verbose = matplotlib.verbose
@@ -12,6 +17,10 @@ verbose = matplotlib.verbose
 # in the future i'm going to implement more usefull features like redraw after changing parameters
 
 
+def get_last_chart_idx():
+    return len([name for name in os.listdir(CHART_DIR) if os.path.isfile(os.path.join(CHART_DIR, name))])
+
+
 def draw_if_interactive():
     if matplotlib.is_interactive():
         figManager = Gcf.get_active()
@@ -19,31 +28,33 @@ def draw_if_interactive():
             figManager.show()
 
 
-class Show(ShowBase):
-    def __call__(self, block=None):
-        managers = Gcf.get_all_fig_managers()
-        if not managers:
-            return
-
-        for manager in managers:
-            manager.show()
+show = ShowBase()
 
 
-show = Show()
+def new_figure_manager(num, *args, **kwargs):
+    FigureClass = kwargs.pop('FigureClass', Figure)
+    figure = FigureClass(*args, **kwargs)
+    return new_figure_manager_given_figure(num, figure)
+
+
+def new_figure_manager_given_figure(num, figure):
+    canvas = FigureCanvasInterAgg(figure)
+    manager = FigureManagerInterAgg(canvas, num)
+    if matplotlib.is_interactive():
+        manager.show()
+    return manager
 
 
 class FigureCanvasInterAgg(FigureCanvasAgg):
     def __init__(self, figure):
         FigureCanvasAgg.__init__(self, figure)
 
-    def draw(self):
-        self.show()
-
-    def draw_idle(self):
-        self.draw()
+    def get_default_filetype(self):
+        return 'png'
 
     def show(self):
-        FigureCanvasAgg.print_png(self, "WHOOP.png")
+        chart_last_idx = get_last_chart_idx()
+        FigureCanvasAgg.print_png(self, NAME_PREFIX + str(chart_last_idx))
 
 
 class FigureManagerInterAgg(FigureManagerBase):
@@ -54,6 +65,15 @@ class FigureManagerInterAgg(FigureManagerBase):
         self._shown = False
 
     def show(self):
-        if not self._shown:
-            self.canvas.show()
-        self._shown = True
+        self.canvas.show()
+        Gcf.destroy(self._num)
+
+
+########################################################################
+#
+# Now just provide the standard names that backend.__init__ is expecting
+#
+########################################################################
+
+FigureCanvas = FigureCanvasInterAgg
+FigureManager = FigureManagerInterAgg
--- interactive_matplotlib/test_interactive.py
+++ interactive_matplotlib/test_interactive.py
@@ -1,11 +1,13 @@
 import interactive_matplotlib
+import matplotlib
 from matplotlib import pyplot as plt
+print matplotlib.get_backend()
 
 plt.plot([0, 1, 2, 3, 4, 5, 6])
 plt.show()
 
-# plt.plot([0, 1, 2, 3, 4, 5], [0, 1, 4, 9, 16, 25])
-# plt.show()
-#
-# plt.plot([0, 1, 2, 3, 4, 5, 6], [0, 1, 1, 1, 1, 1, 1])
-# plt.show()
+plt.plot([0, 1, 2, 3, 4, 5], [0, 1, 4, 9, 16, 25])
+plt.show()
+
+plt.plot([0, 1, 2, 3, 4, 5, 6], [1, 1, 1, 1, 1, 1, 1])
+plt.show()
